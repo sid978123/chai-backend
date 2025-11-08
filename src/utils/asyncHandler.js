a4e273bacd7e8850b27746514189f05abc87ce59
sid978123 <siddharthaman978@gmail.com>
2025-11-08 15:45:38 +0530

    util code discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+//another asyncHandler method to define it...
+
+// const asyncHandler = (fn) =>{ async (req, res, next) => {
+//     try{
+//         await fn( req , res ,next)
+
+//     }catch(error){
+//         res.status(err.code || 500).json({
+//             success : false,
+//             message : err.message
+//         })
+//     }
+// } }
+
+//HIGHER ORDER FUNCTION => that function which can accept functions as parameter and can return it ...
