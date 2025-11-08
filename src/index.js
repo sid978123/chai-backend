b664383349eb29df2c0d8c4f0f639e08c7174f1e
sid978123 <siddharthaman978@gmail.com>
2025-11-08 22:14:36 +0530

    add user and video model

--- src/index.js
+++ src/index.js
@@ -7,7 +7,20 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERROR", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO  db connnection failed !!!", err);
+  });
 
 /* .then(() => {
     app.listen(process.env.PORT || 8000, () => {
