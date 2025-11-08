a911cbc6fcaece056a4e2aab77289746c700a4e7
sid978123 <siddharthaman978@gmail.com>
2025-11-09 00:02:14 +0530

    how to upload image

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localfilePath) => {
+  try {
+    if (!localfilePath) return null;
+    //upload file on cloudinary,...
+    const response = await cloudinary.uploader.upload(localfilePath, {
+      resource_type: "auto",
+    });
+    //file has been uploaded successfully...
+    console.log("file is uploaded successfully");
+    console.log(response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localfilePath); //remove the locally saved temperoary files as the upload operation got failed
+
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
