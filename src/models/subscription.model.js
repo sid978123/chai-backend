ac39876ad4424732ab52869c6aa93fcc4bbb2d93
sid978123 <siddharthaman978@gmail.com>
2025-11-23 13:09:34 +0530

    Refactor subscription model: clean up commented code and improve comments for clarity

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,17 +1,37 @@
-import mongoose, { Schema } from "mongoose";
+// import mongoose, { Schema } from "mongoose";
 
+const { Schema } = require("mongoose");
+
+// const subscriptionSchema = new Schema(
+//   {
+//     subscriber: {
+//       type: Schema.Types.ObjectId, // one who is subscribing
+//       ref: "User",
+//     },
+//     channel: {
+//       type: Schema.Types.ObjectId, // one to whom subscriber is  subscribing
+//       ref: "User",
+//     },
+//   },
+//   { timestamps: true }
+// );
+
+// export const Subscription = mongoose.model("Subscription", subscriptionSchema);
+
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 const subscriptionSchema = new Schema(
   {
     subscriber: {
-      type: Schema.Types.ObjectId, // one who is subscribing
+      type: Schema.Types.ObjectId, // the one who is subscribing
       ref: "User",
     },
     channel: {
-      type: Schema.Types.ObjectId, // one to whom subscriber is  subscribing
+      type: Schema.Types.ObjectId, // one to whom subscriber is subscribing...
       ref: "User",
     },
   },
   { timestamps: true }
 );
 
-export const Subscription = mongoose.model("Subscription", subscriptionSchema);
+const Subscription = mongoose.model("Subscription ", subscriptionSchema);
